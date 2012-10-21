d773431b1526bcc8f8dbedd38632c14a454c902b
Patrick Ng <ckipel@gmail.com>
2012-10-21 05:15:55 -0400

    Removed outdated migrations

--- db/migrate/20121021023213_remove_categories_table.rb
+++
@@ -1,5 +0,0 @@
-class RemoveCategoriesTable < ActiveRecord::Migration
-  def change
-    drop_table :categories
-  end
-end
--- db/migrate/20121021023425_remove_category_table.rb
+++
@@ -1,5 +0,0 @@
-class RemoveCategoryTable < ActiveRecord::Migration
-  def change
-    drop_table :category
-  end
-end
