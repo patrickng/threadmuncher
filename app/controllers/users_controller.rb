9c3912c6ac97f3b18cfa3ec82a1f529795d7c27b
Patrick Ng <ckipel@gmail.com>
2012-10-21 05:47:23 -0400

    Fixed twitter tweets on profile page

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -1,4 +1,5 @@
 class UsersController < ApplicationController
+
   def new
     @user = User.new
   end
@@ -6,6 +7,7 @@ class UsersController < ApplicationController
   def create
     @user = User.new(params[:user])
     if @user.save
+      session[:referer] = request.env["HTTP_REFERER"]
       redirect_to root_url, notice: "Sign up successful!"
     else
       render "new"
@@ -17,16 +19,12 @@ class UsersController < ApplicationController
     @recent_posts = @user.posts.order("created_at desc").limit("10")
     @recent_comments = @user.comments.order("created_at desc").limit("10")
     @recent_voted_posts = @user.votes.order("created_at desc").limit("10").map(&:post).flatten
-  end
 
-  def tweet
     @client = Twitter::Client.new(
       consumer_key: "U1UQAdKnAQU2iNeqAZ7sqg",
       consumer_secret: "X1gimnx2rG7WGNDAcMFCjYTliinrkYZo7e6hrTp8iQ",
       oauth_token: "119667769-PqkTXB247kMpeuV9JPgoQyJWq2l3jzVXznmQVSpO",
       oauth_token_secret: "Tdtq5lpJZWM09eSviKFRfoDBgjE4Qs5sI2FlfCdRIo"
     )
-
-    @timeline = @client.user_timeline(@user.twitter_handle, count: "10")
   end
 end
