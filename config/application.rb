68233d6c2c3486aa4fab64942119c164da216941
Patrick Ng <ckipel@gmail.com>
2013-03-18 15:08:07 -0400

    fixing precompile on production

--- config/application.rb
+++ config/application.rb
@@ -56,6 +56,8 @@ module CourseProject
     # Version of your assets, change this if you want to expire all your assets
     config.assets.version = '1.0'
 
+    config.assets.initialize_on_precompile = false
+
     config.generators do |g|
       g.test_framework :rspec,
         fixtures: true,
