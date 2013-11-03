9e7e68aa3c97efbae5bc52378bbcbd24fdbf95f1
Patrick Ng <ckipel@gmail.com>
2013-11-03 16:29:08 -0500

    formatting the post serializer and JSON format

--- app/serializers/post_serializer.rb
+++ app/serializers/post_serializer.rb
@@ -1,5 +1,13 @@
 class PostSerializer < ActiveModel::Serializer
-  attributes :id, :title, :url, :body, :type_of_post
-  has_many :comments
-  has_one :user, key: :author
+  attributes :id, :title, :url, :body, :type_of_post, :num_comments, :upvotes, :downvotes, :author, :created_at
+  # attribute :num_comments, key: :number_of_comments
+  # has_one :user, key: :author
+
+  def num_comments
+    object.comments.size
+  end
+
+  def author
+    object.user.handle
+  end
 end
