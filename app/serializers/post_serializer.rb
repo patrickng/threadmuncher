8be79d52452604f65cccec7c34cbc99064cc5a8d
Patrick Ng <ckipel@gmail.com>
2013-11-03 23:36:32 -0500

    adding category to JSON response

--- app/serializers/post_serializer.rb
+++ app/serializers/post_serializer.rb
@@ -1,5 +1,5 @@
 class PostSerializer < ActiveModel::Serializer
-  attributes :id, :title, :url, :body, :type_of_post, :num_comments, :upvotes, :downvotes, :author, :created_at
+  attributes :id, :title, :url, :body, :type_of_post, :category, :num_comments, :upvotes, :downvotes, :author, :created_at
   # attribute :num_comments, key: :number_of_comments
   # has_one :user, key: :author
 
@@ -9,5 +9,9 @@ class PostSerializer < ActiveModel::Serializer
 
   def author
     object.user.handle
+  end
+
+  def category
+    object.category.name
   end
 end
