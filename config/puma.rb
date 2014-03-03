15417a9c5b8f07fa10f3d398790c116c03ac5fb9
Patrick Ng <ckipel@gmail.com>
2014-03-02 22:13:18 -0500

    puma config

--- config/puma.rb
+++ config/puma.rb
@@ -3,8 +3,8 @@ codename = 'threadmuncher'
 root = "/home/#{user}/ruby/#{codename}"
 
 daemonize true 
-bind "unix://#{root}/shared/tmp/sockets" 
-pidfile "#{root}/shared/tmp/pids"
-rackup "#{root}/current/config.ru"
+# bind "unix://#{root}/shared/tmp/sockets"
+# pidfile "#{root}/shared/tmp/pids"
+# rackup "#{root}/current/config.ru"
 threads 4, 8  
 activate_control_app
